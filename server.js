d113f5aa10b614290ac3bc3f93bbc6df749f9efb
Abhinav Lakshmanan <46086523+ruralad@users.noreply.github.com>
2022-02-27 12:54:13 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+const fs = require("fs");
+const { response } = require("express");
+const app = express();
+const port = 5500;
+
+const configuration = new Configuration({
+  apiKey: "",
+});
+const openai = new OpenAIApi(configuration);
+
+app.get("/", async (req, res) => {
+      const response = await openai.createCompletion("text-davinci-001", {
+        prompt: "tell me a story that's happy and not too cliche",
+        temperature: 0.7,
+        max_tokens: 234,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+   const obj = fs.readFileSync("./data.json",{encoding :"utf-8"});
+   let file = JSON.parse(obj);
+   file.stories.push(response.data.choices[0].text)
+   fs.writeFileSync("./data.json",JSON.stringify(file))
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
