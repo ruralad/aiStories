68c9b335396c3d61b7e1c3d6a8da418f29185248
Abhinav Lakshmanan <46086523+ruralad@users.noreply.github.com>
2022-03-05 22:27:08 +0530

    localStorage error fix-1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {useLocalStorage} from "use-local-storage";
+import useLocalStorage from "use-local-storage";
 import "./styles/App.css";
 
 import book from "./assets/book.png";
@@ -38,7 +38,7 @@ export default function App() {
         <h2>aistories? gpt-3?</h2>
         <p>basically i wanted to do something with gpt-3 (<a href="https://en.wikipedia.org/wiki/GPT-3" target="__blank">wikipedia</a> : 'a autoregressive language model that uses deep learning to produce human-like text'). aistories contains short stories writted by gpt-3, updated daily.</p>
         <h2>how does it work</h2>
-        <p>in nutshell, new stories are made midnight every day in a nodejs server using openai api, and the results are stores in firebase database. code is <a href="https://github.com/ruralad/openai-story-project" target="__blank">open source</a>  btw.</p>
+        <p>in a nutshell, new stories are made midnight every day in a nodejs server using openai api, and the results are stores in firebase database. code is <a href="https://github.com/ruralad/openai-story-project" target="__blank">open source</a>  btw.</p>
       </div>
     );
   }
