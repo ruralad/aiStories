39d93b2dd4a60cd45c181db0e7d5b69bb1a548fb
Abhinav Lakshmanan <46086523+ruralad@users.noreply.github.com>
2022-03-18 18:35:52 +0530

    Update Today.jsx

--- client/src/Today.jsx
+++ client/src/Today.jsx
@@ -12,6 +12,9 @@ export default function Today() {
     "theme",
     defaultDark ? "dark" : "light"
   );
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
     return (
       <div className={styles.app} data-theme={theme}>
       {!info && <TopBar theme={theme} setInfo={false} setTheme={setTheme} />}
