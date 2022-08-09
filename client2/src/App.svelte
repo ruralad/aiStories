d1016181e1c23bff208e5ae09d8ae0a1a12b8f5b
ruralad <avabhinav13@gmail.com>
2022-08-09 21:36:06 +0530

    svelte test?

---
+++ client2/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Link, Route, Router } from "svelte-routing";
+  import About from "./routes/About.svelte";
+  import Home from "./routes/Home.svelte";
+
+  export let url = "";
+</script>
+
+<Router {url}>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+  </nav>
+  <div>
+    <Route path="about" component={About} />
+    <Route path="/" component={Home} />
+  </div>
+</Router>
---
+++ client2/src/routes/About.svelte
@@ -0,0 +1,3 @@
+<main>
+  <h1>about</h1>
+</main>
